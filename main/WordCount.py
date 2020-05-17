7b72f59b64129ac1602cefa9796d9c5dac8913ac
vik2595 <vik2595@hotmail.com>
2020-05-17 16:36:17 -0400

    Initial commit

---
+++ main/WordCount.py
@@ -0,0 +1,45 @@
+#Finding top 5 used words from Sample.txt and it's count. Print bar chart for top 5 most popular words.
+
+from nltk.tokenize import RegexpTokenizer
+from nltk.probability import FreqDist
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open(r'Sample.txt') as f:
+    lines = f.read()
+
+file_tokenizer = RegexpTokenizer(r'\w+')
+tokens = file_tokenizer.tokenize(lines)
+
+total_words = len(tokens)
+type1 = type(tokens)
+print("Total number of words in the file : {0}".format(total_words))
+
+#finding top 5 words from text file
+main_list = FreqDist(tokens)
+top_five = main_list.most_common(5)
+
+
+#printing word and wordcount
+count = 0
+for i in top_five:
+    count += 1
+    print("Number {0} word and total wordcount is {1}".format(count,i))
+
+
+# code to split it into 2 lists for chart purpose
+wlist = [i[0] for i in top_five]
+clist = [i[1] for i in top_five]
+
+#For bar chart
+y_pos = np.arange(len(wlist))
+plt.bar(y_pos, clist, align='center', alpha=0.5)
+plt.xticks(y_pos, wlist)
+plt.ylabel('Total words in text')
+plt.title('Word')
+
+for i in range(len(clist)):
+    plt.text(x = clist[i]-0.5 , y = wlist[i]+0.1, s = clist[i], size = 6)
+
+plt.show()
